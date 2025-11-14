e01262d9ea0758c2b94a66761a89fa5fe886662f
Emilio <emistien@gmail.com>
2025-11-14 22:43:29 +0100

    q1.1.3 done

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Projet_LTPF
+
+## 1 - Préliminiaires théoriques
+
+### 1.1.1 Définition et analyse d’un langage de programmation simple
+
+Une première Grammaire (récursive à gauche indirecte) : 
+
+  Grammaire :
+
+  **Var** ::= 'a' | 'b' | 'c' | 'd'<br>
+  **Cst** ::= 0 | 1<br>
+  **Expr** ::= Cst | Var<br>
+  **Instr** ::= Assign | Seq | If | While | epsilon<br>
+  **Assign** ::= Var ':' '=' Expr<br>
+  **Seq** ::= Instr ';' Instr <br>
+  **If** ::= 'i' '(' Var ')' '{' Prog '}' '{' Prog '}'<br>
+  **While** ::= 'w' '(' Var ')' '{' Prog '}'<br>
+  **Prog** ::= Prog ';' Instr | Instr<br>
+
+
+->  En effet, lorsque qu'on appelle une instruction, cela peut
+    être une séquence qui elle même est constituée de suites d'instruction ect. <br>
+==> On s'embarque dans une boucle infinie.
+
+
+### 1.1.2 Grammaire non récursive à gauche 
+
+
+  Grammaire :
+
+  **Var** ::= 'a' | 'b' | 'c' | 'd'<br>
+  **Cst** ::= 0 | 1<br>
+  **Expr** ::= Cst | Var<br>
+  **Instr** ::=  Assign | If | While<br>
+  **InstrSuite** ::= ';' Instr InstrSuite | epsilon<br>
+  **Assign** ::= Var ':' '=' Expr<br>
+  **If** ::= 'i' '(' Var ')' '{' Prog '}' '{' Prog '}'<br>
+  **While** ::= 'w' '(' Var ')' '{' Prog '}'<br>
+  **Prog** ::= Instr InstrSuite <br>
+
+
+
+
+
--- projet.ml
+++ projet.ml
@@ -6,12 +6,12 @@ type expr =
 |EConst of int (*0 ou 1*)
 |EVar of var;; (*variables a b c ou d*)
 
-type prog = 
+type instr = 
 |Skip
 |Assign of var * expr
-|Seq of prog * prog
-|If of var * prog * prog
-|While of var * prog;;
+|Seq of instr * instr
+|If of var * instr * instr
+|While of var * instr;;
 
 
 (*Exercice 1.1.2 *)
@@ -22,15 +22,38 @@ type prog =
   Var ::= 'a' | 'b' | 'c' | 'd'
   Cst ::= 0 | 1
   Expr ::= Cst | Var
-  Instr ::= Skip | Assign | Seq | If | While
+  Instr ::= Assign | Seq | If | While | epsilon
   Assign ::= Var ':' '=' Expr
   Seq ::= Instr ';' Instr 
   If ::= 'i' '(' Var ')' '{' Prog '}' '{' Prog '}'
   While ::= 'w' '(' Var ')' '{' Prog '}'
-  Prog ::= Instr
+  Prog ::= Prog ';' Instr | Instr
 
+  
+  (*==> Notre grammaire est récursive a gauche : 
+        Un programme (qui est une suite d'instruction) peut se répeter 
+        à l'infini). 
+    *)
 
 *)
 
 (*Exercice 1.1.3 *)
+
+(* Nouvelle grammaire :
+
+  Var ::= 'a' | 'b' | 'c' | 'd'
+  Cst ::= 0 | 1
+  Expr ::= Cst | Var
+  Instr ::=  Assign | If | While
+  InstrSuite ::= ';' Instr InstrSuite | epsilon
+  Assign ::= Var ':' '=' Expr
+  If ::= 'i' '(' Var ')' '{' Prog '}' '{' Prog '}'
+  While ::= 'w' '(' Var ')' '{' Prog '}'
+  Prog ::= Instr InstrSuite 
+
+*)
+
+
+
+
 
